a92db0c39aab91b2a34fbcb479a12da214a0e93e
Lakshman <lkolasan@uncc.edu>
2017-03-20 13:37:03 -0400

    Update TextAnalytics_NIH_code.R

--- TextAnalytics_NIH_code.R
+++ TextAnalytics_NIH_code.R
@@ -1,11 +1,20 @@
+#Reading psychcentral data
 pcd <- fread("psychcentral_data.csv", sep=",", header=T, strip.white = T, na.strings = c("NA","NaN","","?"))
 str(pcd)
+
+#Loading libraries “dplyr” and “tidytext” to tokenize column q_content
 library(dplyr)
 library(tidytext)
 tidy_text <- pcd %>% unnest_tokens(word, q_content) 
 tidy_text[1:5]
+
+#Removing stopwords
 tidy_text <- tidy_text %>% anti_join(stop_words)
+
+#Sorting by frequency of tokens
 tidy_text %>% count(word, sort = TRUE)
+
+#Visualizing sorted tokens with min freq of 2000
 library(ggplot2)
 tidy_text %>% 
   count(word, sort = TRUE) %>%
@@ -15,6 +24,9 @@ tidy_text %>%
   geom_bar(stat = "identity") +
   xlab(NULL) +
   coord_flip()
-  
+
+#Stemmng the orginal data
 install.packages("SnowballC", repos = "https://cran.r-project.org")
+library(SnowballC)
+tidy_text <- pcd %>% unnest_tokens(word, q_content) %>% mutate(word = wordStem(word)) 
 
